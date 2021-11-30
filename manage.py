03f45b8ea70fe0584d35c256a73d27193c5c7b75
stanleykelder <stanleykelder@gmail.com>
2021-11-30 13:41:53 +0100

    Reset

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myqrtoolkit.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_qr_toolkit_models.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
