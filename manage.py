80bb3255a8c816e02c172ada6fdce9979aff0958
stanleykelder <stanleykelder@gmail.com>
2021-11-30 12:08:42 +0100

    Try for Heroku 2

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_qr_toolkit_models.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myqrtoolkit.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 
 if __name__ == '__main__':
