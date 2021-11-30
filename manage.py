ed24ded3957b8efd5c927df0cd41cee3ddb8126d
stanleykelder <stanleykelder@gmail.com>
2021-11-30 11:32:39 +0100

    Try for Heroku

--- manage.py
+++ manage.py
@@ -15,7 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(['python3', '-m', 'pip', 'install', 'django-qr-toolkit-core'])
+
 
 
 if __name__ == '__main__':
