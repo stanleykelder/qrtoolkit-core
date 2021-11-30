d8876e655ddbb25fa25a74fbd8ddaff8c4a61393
stanleykelder <stanleykelder@gmail.com>
2021-11-30 17:26:22 +0100

    -

--- myqrtoolkit/settings.py
+++ myqrtoolkit/settings.py
@@ -1,3 +1,6 @@
+# Added for Heroku (https://devcenter.heroku.com/articles/django-app-configuration)
+import django_heroku
+
 """
 Django settings for myqrtoolkit project.
 
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
