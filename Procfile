c2cb3ecbd5203c4e685072d89de8b04afdd06a80
stanleykelder <stanleykelder@gmail.com>
2022-08-10 16:23:29 +0200

    try to change the toolkit to the new version

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
 web: gunicorn myqrtoolkit.wsgi
+heroku ps:scale web=1
--- qrtoolkit_core/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QrtoolkitModelsConfig(AppConfig):
-    name = 'qrtoolkit_models'
